fd300378a609e5201208cc5da15855bb7c82c34a
Buratinka <oleg.shved.shvets@gmail.com>
2019-06-10 22:03:01 +0300

    test commit

---
+++ src/main/java/com/three_developers_team/spring_presentation_team_application_restaurant/SpringPresentationTeamApplicationRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.three_developers_team.spring_presentation_team_application_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPresentationTeamApplicationRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPresentationTeamApplicationRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/three_developers_team/spring_presentation_team_application_restaurant/SpringPresentationTeamApplicationRestaurantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.three_developers_team.spring_presentation_team_application_restaurant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPresentationTeamApplicationRestaurantApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
