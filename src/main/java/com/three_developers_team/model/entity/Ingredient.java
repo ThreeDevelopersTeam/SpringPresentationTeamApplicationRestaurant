21e5ccebc01affb7517dec414f880cdc51b27101
antonplatov <xanton.platovx@gmail.com>
2019-06-11 13:13:47 +0300

    created Ingredient.java

--- src/main/java/com/three_developers_team/model/entity/Ingredient.java
+++ src/main/java/com/three_developers_team/model/entity/Ingredient.java
@@ -1,4 +1,30 @@
 package com.three_developers_team.model.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredient")
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ingredient")
+    private Long id;
+
+
+
+    @Column(name = "name")
+    private String name;
+
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_dish")
+    private Dish dish;
+
+
+
 }
