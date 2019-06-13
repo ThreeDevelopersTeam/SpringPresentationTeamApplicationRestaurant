f285830ebef046ce657a0f70728f14f3d1d28d62
Oleksandr Borodavka <aleks.borodavka@ukr.net>
2019-06-13 13:54:18 +0300

    Files was moved to application base pachage for all modules (moved to three_developers_team), because by default Spring Boot makes component scan in the packages where it is located.

--- src/main/java/com/three_developers_team/SpringPresentationTeamApplicationRestaurantApplication.java
+++ src/main/java/com/three_developers_team/SpringPresentationTeamApplicationRestaurantApplication.java
@@ -1,4 +1,4 @@
-package com.three_developers_team.spring_presentation_team_application_restaurant;
+package com.three_developers_team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
